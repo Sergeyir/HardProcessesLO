5274d4f24ab36e129cc8c8359a0aefb4a26d44ca
Sergeyir <antsupov0124@gmail.com>
2025-11-05 01:28:41 +0300

    adding cross section calculations examples

--- include/CalculateAnalytic.hpp
+++ include/CalculateAnalytic.hpp
@@ -12,6 +12,8 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cmath>
+#include <chrono>
 
 #include "LHAPDF/LHAPDF.h"
 
@@ -19,30 +21,72 @@
 
 #include "TFile.h"
 #include "TH1D.h"
+#include "TRandom.h"
 
 #include "Constants.hpp"
 
-/* @brief Calculates x for the first parton for the given pT, \sqrt{s_{NN}}, y1, y2
+/// LHDAPDF::PDF object; it is used to obtain \alpha_Q, Q1, and Q2 
+/// if initialized for the given pdf set
+LHAPDF::PDF *pdf;
+/// ROOT TRandom object; it is used to randomly select y1, y2, and p_T for MC integration
+TRandom rnd;
+/// number of integration steps for MC integration
+long numberOfIntegrationSteps;
+
+/* @brief Returns random number that can be used as seed for TRandom
+ */
+unsigned int GetRandomSeed();
+/* @brief Calculates d\sigma / d\Omega for all processes id1+id2 -> X + X for the given pT, \sqrt{s'_{NN}}, and y
+ *
+ * @param[in] id1 id of a 1st parton
+ * @param[in] id2 id of a 2nd parton
+ * @param[in] pT transverse momentum [GeV/c]
+ * @param[in] s square root of s'_{NN} (' denotes parton COM frame) [GeV]
+ * @param[in] y = y1 - y2
+ * @param[out] d\sigma / dp_T dy_1 dy_2
+ */
+double GetDSigmaDOmega(const int id1, const int id2, const double pT, 
+                       const double s, const double y);
+/* @brief Calculates d\sigma / dp_T dy_1 dy_2 for the given pT, \sqrt{s'_{NN}}, y1, y2, x1, and x2
  *
  * @param[in] pT transverse momentum [GeV/c]
- * @param[in] sNNSqrt square root of s_{NN}
+ * @param[in] s square root of s'_{NN} (' denotes parton COM frame) [GeV]
+ * @param[in] y1 1st parton rapidity
+ * @param[in] y2 2nd parton rapidity
+ * @param[in] x1 1st parton x
+ * @param[in] x2 2nd parton x 
+ * @param[out] d\sigma / dp_T dy_1 dy_2
+ */
+double GetDSigmaDPTDY1DY2(const double pT, const double s, 
+                          const double y1, const double y2,
+                          const double x1, const double x2);
+/* @brief Calculates x (parton momentum fraction alongside Z axis) for the first parton for the given pT, \sqrt{s_{NN}}, y1, y2
+ *
+ * @param[in] pT transverse momentum [GeV/c]
+ * @param[in] sqrtSNN square root of s_{NN} [GeV]
  * @param[in] y1 rapidity of a first parton
  * @param[in] y2 rapidity of a second parton
+ * @param[out] x
  */
-double GetX1(const double pT, const double sNNSqrt, const double y1, const double y2);
-/* @brief Calculates x for the second parton for the given pT, \sqrt{s_{NN}}, y1, y2
+double GetX1(const double pT, const double sqrtSNN, const double y1, const double y2);
+/* @brief Calculates x (parton momentum fraction alongside Z axis) for the second parton for the given pT, \sqrt{s_{NN}}, y1, y2
  *
  * @param[in] pT transverse momentum [GeV/c]
- * @param[in] sNNSqrt square root of s_{NN}
+ * @param[in] sqrtSNN square root of s_{NN} [GeV]
  * @param[in] y1 rapidity of a first parton
  * @param[in] y2 rapidity of a second parton
+ * @param[out] x
  */
-double GetX2(const double pT, const double sNNSqrt, const double y1, const double y2);
-/* @brief Calculates d\sigma / dp_T for the given pT
+double GetX2(const double pT, const double sqrtSNN, const double y1, const double y2);
+/* @brief Calculates d\sigma / dp_T for the given pT, \sqrt{s_{NN}} and rapidity range. Integration will be performed over [-absYMax, absYMax] range for both y1 and y2
  *
  * @param[in] pT transverse momentum [GeV/c]
+ * @param[in] sqrtSNN square root of s_{NN} [GeV]
+ * @param[in] absYMax maximum absolute of a rapidity of a parton
+ * @param[in] err statistic uncertainty tied to limited number of integration steps
+ * @param[out] d\sigma / dp_T
  */
-double GetDSigmaDPT(const double pT);
+double GetDSigmaDPT(const double pT, const double sqrtSNN, const double absYMax, double &err);
 
 int main(int argc, char **argv);
 
--- src/CalculateAnalytic.cpp
+++ src/CalculateAnalytic.cpp
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
       std::cout << COLOR::BOLD_RED << "Error: " << COLOR::RESET << 
                    "Expected 2 parameters while " + std::to_string(argc - 1) + " "\
                    "parameter(s) were provided \n Usage: bin/CalculateAnalytic "\
-                   "inputFileName.yaml numberOfMCTries" << std::endl;
+                   "inputFileName.yaml numberOfIntegrationSteps" << std::endl;
       std::cout << "[" << COLOR::BOLD_GREEN << "INFO" << COLOR::RESET << "] " << 
                    " input file example is located in input directory" << std::endl;
       return 1;
@@ -35,10 +35,134 @@ int main(int argc, char **argv)
       return 1;
    }
 
+   numberOfIntegrationSteps = std::stol(argv[2]);
+
    // YAML::Node object reads all data from the .yaml file
    YAML::Node inputFileContents = YAML::LoadFile(argv[1]);
 
+   const std::string pdfSet = inputFileContents["pdfset"].as<std::string>();
+   const double pTHatMin = inputFileContents["pthatmin"].as<double>();
+   const double absMaxY = inputFileContents["abs_max_y"].as<double>();
+   const double sNN = inputFileContents["energy"].as<double>();
+
+   pdf = LHAPDF::mkPDF(pdfSet);
+   rnd.SetSeed(GetRandomSeed());
+
+   TFile outputFile("output/analytic.root", "RECREATE");
+
+   TH1D distrDSigmaDPT("dsigma/dpT", "d\\Sigma/dp_{T}", 1000 + pTHatMin, 0., 100. + pTHatMin);
+
+   for (int i = 1; i <= distrDSigmaDPT.GetXaxis()->GetNbins(); i++)
+   {
+      double err;
+      distrDSigmaDPT.SetBinContent(i, GetDSigmaDPT(distrDSigmaDPT.GetXaxis()->GetBinCenter(i), 
+                                   sqrt(sNN), absMaxY, err));
+      distrDSigmaDPT.SetBinError(i, err);
+   }
+
+   distrDSigmaDPT.Write();
+
+   outputFile.Close();
+
    return 0;
+}
+
+double GetDSigmaDOmega(const int id1, const int id2, const double pT, 
+                       const double s, const double y)
+{
+   // to do: define mandelstam variables (t, u) and substitute lower random dummies for them
+   // (hint: it will be easier if you define cos(\Theta) first)
+   const double t = rnd.Uniform(-s, s);
+   const double u = rnd.Uniform(-s, s);
+   
+   // obtaining alpha_{S} at hard scale, i.e. \mu_F = p_T
+   const double alphaS = pdf->alphasQ2(pT*pT);
+
+   // only qq->qq is shown below as an example
+   if (id1 != 0 && id2 != 0 && id1 == id2)
+   {
+      // source: Eq. 51.7 at https://pdg.lbl.gov/2025/reviews/contents_sports.html
+      return alphaS*alphaS/(9.*s)*((t*t + s*s)/(u*u) + (s*s + u*u)/(t*t) - 2.*s*s/(3.*u*t));
+   }
+   // to do: add other d\sigma / d\Omega processes here
+
+   return 0.;
+}
+
+double GetDSigmaDPTDY1DY2(const double pT, const double s, 
+                          const double y1, const double y2, 
+                          const double x1, const double x2)
+{
+   // result of a sum over all combinations
+   double result = 0.;
+   
+   // iterating over all posible combinations
+   for (int id1 = -5; id1 <= 5; id1++)
+   {
+      for (int id2 = -5; id2 <= 5; id2++)
+      {
+         // 1e9 converts mb into pb
+         result += 8.*M_PI*pT*pdf->xfxQ2(id1, x1, pT*pT)*pdf->xfxQ2(id2, x2, pT*pT)*
+                   GetDSigmaDOmega(id1, id2, pT, s, y1 - y2)/s*1e9;
+      }
+   }
+   return result;
+}
+
+double GetX1(const double pT, const double sqrtSNN, const double y1, const double y2)
+{
+   return 2.*pT/sqrtSNN*exp(-1./2.*(y1 - y2))*cosh((y1 - y2)/2.);
+}
+
+double GetX2(const double pT, const double sqrtSNN, const double y1, const double y2)
+{
+   // to do: fill in the formula for x2 in the current function
+   return 0.5;
+}
+
+double GetDSigmaDPT(const double pT, const double sqrtSNN, const double absYMax, double &err)
+{
+   // result of MC integration
+   double result = 0.;
+   // normalization constant for MC integration: equals to the number of succesfull integration steps
+   long normalization = 0;
+
+   for (long i = 0; i < numberOfIntegrationSteps; i++)
+   {
+      // randomly choosing y1 and y2
+      const double y1 = rnd.Uniform(-absYMax, absYMax);
+      const double y2 = rnd.Uniform(-absYMax, absYMax);
+
+      // to do: introduce a check that tests if pT exceeds kinematicaly allowed maximum value
+
+      // calculating x1 and x2
+      const double x1 = GetX1(pT, sqrtSNN, y1, y2);
+      const double x2 = GetX2(pT, sqrtSNN, y1, y2);
+
+      // to do: add a check that tests whether x1 and x2 are within kinematicaly possible range
+
+      // calculating \sqrt{S'_{NN}}, i.e. the center of mass energy of 2 partons
+      const double s = sqrtSNN*x1*x2;
+
+      result += GetDSigmaDPTDY1DY2(pT, s, y1, y2, x1, x2);
+      normalization++;
+   }
+
+   // scaling integration sum by the number of succesfull integration steps
+   result /= static_cast<double>(normalization);
+
+   err = result/sqrt(normalization);
+
+   return result;
+}
+
+unsigned int GetRandomSeed()
+{
+	auto now = std::chrono::high_resolution_clock::now();
+	auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
+	auto epoch = now_ms.time_since_epoch();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
+	return static_cast<unsigned int>(duration.count() % 900000000);
 }
 
 #endif /* CALCULATE_ANALYTIC_CPP */
